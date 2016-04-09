5838b261cc8bb41db8541316b3fcd9bce72dccb9
zefa <wangwei@youzan.com>
2016-04-09 17:58:50 +0800

    单号生成器

---
+++ src/main/java/com/youzan/sz/common/util/NumberUtils.java
@@ -0,0 +1,32 @@
+package com.youzan.sz.common.util;
+
+import com.youzan.sz.common.model.number.NumberTypes;
+import org.joda.time.DateTime;
+
+import java.util.Random;
+
+/**
+ * Created by zefa on 16/4/9.
+ */
+public class NumberUtils {
+    public static String initNumber(NumberTypes numberType){
+        String timeStamp = new DateTime().toString(numberType.getFormat());
+        int randomNumber = (int)((1 + new Random().nextDouble()) * Math.pow(10, numberType.getNumberLength() - 1));
+        return numberType.getHead() + timeStamp + (randomNumber > 0 ? randomNumber : "");
+    }
+
+    public static void main(String args[]){
+        System.out.println(NumberTypes.PRODUCT.getName() + ":" + NumberUtils.initNumber(NumberTypes.PRODUCT));
+        System.out.println(NumberTypes.SELL.getName() + ":" + NumberUtils.initNumber(NumberTypes.SELL));
+        System.out.println(NumberTypes.RETURN.getName() + ":" + NumberUtils.initNumber(NumberTypes.RETURN));
+        System.out.println(NumberTypes.STOCKCHECK.getName() + ":" + NumberUtils.initNumber(NumberTypes.STOCKCHECK));
+        System.out.println(NumberTypes.PURCHASE.getName() + ":" + NumberUtils.initNumber(NumberTypes.PURCHASE));
+        System.out.println(NumberTypes.STOCKIN.getName() + ":" + NumberUtils.initNumber(NumberTypes.STOCKIN));
+        System.out.println(NumberTypes.STOCKOUT.getName() + ":" + NumberUtils.initNumber(NumberTypes.STOCKOUT));
+        System.out.println(NumberTypes.SHOUKUAN.getName() + ":" + NumberUtils.initNumber(NumberTypes.SHOUKUAN));
+        System.out.println(NumberTypes.FUKUAN.getName() + ":" + NumberUtils.initNumber(NumberTypes.FUKUAN));
+        System.out.println(NumberTypes.YINGSHOUKUAN.getName() + ":" + NumberUtils.initNumber(NumberTypes.YINGSHOUKUAN));
+        System.out.println(NumberTypes.YINGFUKUAN.getName() + ":" + NumberUtils.initNumber(NumberTypes.YINGFUKUAN));
+
+    }
+}
