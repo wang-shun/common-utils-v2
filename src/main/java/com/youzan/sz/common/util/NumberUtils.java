72339e44832b4c3417b7a0e35d553de87c875e8f
Kid <wanghanyang@youzan.com>
2016-04-15 18:19:54 +0800

    增加批量生成条码

--- src/main/java/com/youzan/sz/common/util/NumberUtils.java
+++ src/main/java/com/youzan/sz/common/util/NumberUtils.java
@@ -3,19 +3,34 @@ package com.youzan.sz.common.util;
 import com.youzan.sz.common.model.number.NumberTypes;
 import org.joda.time.DateTime;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Created by zefa on 16/4/9.
  */
 public class NumberUtils {
-    public static String initNumber(NumberTypes numberType){
+    public static String initNumber(NumberTypes numberType) {
         String timeStamp = new DateTime().toString(numberType.getFormat());
-        int randomNumber = (int)((1 + new Random().nextDouble()) * Math.pow(10, numberType.getNumberLength() - 1));
+        int randomNumber = (int) ((1 + new Random().nextDouble()) * Math.pow(10, numberType.getNumberLength() - 1));
         return numberType.getHead() + timeStamp + (randomNumber > 0 ? randomNumber : "");
     }
 
-    public static void main(String args[]){
+    public static List<String> batchGenerateSkuNo(int num) {
+        String timeStamp = DateTime.now().toString(NumberTypes.PRODUCT.getFormat());
+        long millTimes = Long.parseLong(timeStamp);
+        StringBuilder sb = new StringBuilder();
+        List<String> result = new ArrayList<>(num);
+        for (int i = 0; i < num; i++) {
+            result.add(sb.append(NumberTypes.PRODUCT.getHead()).append(timeStamp).toString());
+            sb.delete(0, sb.length());
+            millTimes++;
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
         System.out.println(NumberTypes.PRODUCT.getName() + ":" + NumberUtils.initNumber(NumberTypes.PRODUCT));
         System.out.println(NumberTypes.SELL.getName() + ":" + NumberUtils.initNumber(NumberTypes.SELL));
         System.out.println(NumberTypes.RETURN.getName() + ":" + NumberUtils.initNumber(NumberTypes.RETURN));
