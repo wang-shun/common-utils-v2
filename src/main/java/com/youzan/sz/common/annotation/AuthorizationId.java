c0701b35879578bd64449e01944892472a790823
jinxiaofei <jinxiaofei@youzan.com>
2016-04-19 17:54:25 +0800

    根据鉴权id

---
+++ src/main/java/com/youzan/sz/common/annotation/AuthorizationId.java
@@ -0,0 +1,26 @@
+package com.youzan.sz.common.annotation;
+
+import com.youzan.sz.oa.enums.RoleEnum;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by YANG on 16/4/7.
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthorizationId {
+
+    RoleEnum[] allowedRoles();
+
+    //不传,则从DistributedContext上下文中获取,如果上下文中没有,则返回鉴权失败
+    String adminId() default "";
+
+    // 不传,表示不检验操作人与被操作人的关系
+    String id() default "";
+
+}
