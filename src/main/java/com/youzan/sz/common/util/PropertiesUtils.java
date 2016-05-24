919f8f6b4cab0dbf792e9c0580c66e00b3c97fb7
Kid <wanghanyang@youzan.com>
2016-05-24 16:05:56 +0800

    修改PropertiesUtils.java , 改成class.getResourceAsStream() 读取文件

--- src/main/java/com/youzan/sz/common/util/PropertiesUtils.java
+++ src/main/java/com/youzan/sz/common/util/PropertiesUtils.java
@@ -2,50 +2,57 @@ package com.youzan.sz.common.util;
 
 import com.youzan.platform.bootstrap.exception.BusinessException;
 import com.youzan.sz.common.response.enums.ResponseCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by zefa on 16/4/18.
  */
 public class PropertiesUtils {
-    private static Map<String, Map<String, String>> map = new HashMap<>();
+    private static final ConcurrentHashMap<String, ConcurrentHashMap<String, String>> map = new ConcurrentHashMap<>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesUtils.class);
 
     /**
      * 获取配置
      *
-     * @param propertiesName 配置文件名称
-     * @param propertiesKey  配置名称
+     * @param fileName      配置文件名称
+     * @param propertiesKey 配置名称
      * @return 配置值
      */
-    public static String getProperty(String propertiesName, String propertiesKey) throws BusinessException {
+    public static String getProperty(String fileName, String propertiesKey) throws BusinessException {
         //判断是否命中缓存,命中则从缓存中读取
-        if (map.get(propertiesName) == null) {
+        if (map.get(fileName) == null) {
             //未命中缓存则读取配置文件
             Properties prop = new Properties();
+
+            InputStream inputStream = null;
             try {
-                Map<String, String> propertiesMap = new HashMap<>();
-                //读取属性文件a.properties
-                InputStream in = new BufferedInputStream(new FileInputStream(propertiesName));
-                prop.load(in);     ///加载属性列表
-                Iterator<String> it = prop.stringPropertyNames().iterator();
-                while (it.hasNext()) {
-                    String key = it.next();
+                ConcurrentHashMap<String, String> propertiesMap = new ConcurrentHashMap<>();
+                inputStream = PropertiesUtils.class.getResourceAsStream(fileName);
+                prop.load(inputStream);
+                for (String key : prop.stringPropertyNames()) {
                     propertiesMap.put(key, prop.getProperty(key));
                 }
-                map.put(propertiesName, propertiesMap);
-                in.close();
+                map.put(fileName, propertiesMap);
             } catch (Exception e) {
+                LOGGER.error("Read Property Exception:{}", e);
                 throw new BusinessException((long) ResponseCode.ERROR.getCode(), e.getMessage());
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        LOGGER.error("Read Property Exception:{}", e);
+                    }
+                }
             }
         }
-        return map.get(propertiesName).get(propertiesKey);
+        return map.get(fileName).get(propertiesKey);
     }
 
     /**
