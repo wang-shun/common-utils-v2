101950471db111fd9b53eb575a465b25af4317f5
Kid <wanghanyang@youzan.com>
2016-05-30 15:22:52 +0800

    修复findBug插件 发现的随机数问题

--- src/main/java/com/youzan/sz/common/util/ListUtils.java
+++ src/main/java/com/youzan/sz/common/util/ListUtils.java
@@ -8,29 +8,30 @@ import java.util.List;
  */
 public class ListUtils {
     /**
-     *
      * @param list
      * @return
      */
-    public static String list2String(List list){
-        String result = "";
-        for (Object object: list){
-            result += String.valueOf(object) + ",";
+    public static String list2String(List list) {
+        StringBuilder result = new StringBuilder();
+        for (Object object : list) {
+            result.append(String.valueOf(object));
+            result.append(",");
         }
-        if (result.length() > 0){
-            result = result.substring(0,result.length() - 1);
+        if (result.length() > 0) {
+            return result.substring(0, result.length() - 1);
         }
-        return result;
+        return result.toString();
     }
 
     /**
      * 比较两个list的不同之处
+     *
      * @param firstList
      * @param secondList
      * @param <E>
      * @return 第一个比第二个多的
      */
-    public static <E> List<E> compareTheDifferenceBetween2List(List<E> firstList, List<E> secondList){
+    public static <E> List<E> compareTheDifferenceBetween2List(List<E> firstList, List<E> secondList) {
         List<E> differenceList = new ArrayList<>(firstList);
         for (E elem : secondList) {
             if (firstList.contains(elem))
@@ -41,12 +42,13 @@ public class ListUtils {
 
     /**
      * 比较两个list的相同之处
+     *
      * @param firstList
      * @param secondList
      * @param <E>
      * @return
      */
-    public static <E> List<E> comparisonOfTheTwoListInTheSamePlace(List<E> firstList, List<E> secondList){
+    public static <E> List<E> comparisonOfTheTwoListInTheSamePlace(List<E> firstList, List<E> secondList) {
         List<E> differenceList = new ArrayList<>(firstList);
         for (E elem : secondList) {
             if (!firstList.contains(elem))
--- src/main/java/com/youzan/sz/common/util/RandomUtils.java
+++ src/main/java/com/youzan/sz/common/util/RandomUtils.java
@@ -6,14 +6,18 @@ import java.util.Random;
  * Created by zefa on 16/4/18.
  */
 public class RandomUtils {
+
+    private static final Random RANDOM = new Random();
+
     /**
      * 获取指定位数的随机数
      * (首位1是防止00023变成23导致位数不足,所以请自行去除)
+     *
      * @param figure 随机数位数
      * @return 随机数
      */
     public static int getRandom(int figure) {
-        return (int) ((1 + new Random().nextDouble()) * Math.pow(10, figure));
+        return (int) ((1 + RANDOM.nextDouble()) * Math.pow(10, figure));
     }
 
     /**
@@ -26,8 +30,9 @@ public class RandomUtils {
         int random = getRandom(figure);
         return String.valueOf(random).substring(1);
     }
+
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++)
-        System.out.println(RandomUtils.getRandomNumber(0));
+            System.out.println(RandomUtils.getRandomNumber(0));
     }
 }
