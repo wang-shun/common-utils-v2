f942f3d969d176dafcf9c48fc0f9684e92ad197c
zefa <wangwei@youzan.com>
2016-06-06 10:15:02 +0800

    无需登录的方法

---
+++ src/main/java/com/youzan/sz/common/annotation/WithoutLogging.java
@@ -0,0 +1,17 @@
+package com.youzan.sz.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zefa
+ * Date: 2016/06/06
+ * Time: 13:24
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WithoutLogging {
+}
