eaa74a91dd84b5c23b09724f7e7469bdce285573
zefa <wangwei@youzan.com>
2016-07-07 09:50:17 +0800

    添加RespUtils

---
+++ src/main/java/com/youzan/sz/common/util/ResponseUtils.java
@@ -0,0 +1,15 @@
+package com.youzan.sz.common.util;
+
+import com.youzan.sz.common.response.ResponseBoolean;
+
+/**
+ * Created by zefa on 16/7/7.
+ */
+public class ResponseUtils {
+    private ResponseUtils() {
+        throw new IllegalAccessError("Utility class");
+    }
+
+    public static final ResponseBoolean TRUE = new ResponseBoolean(Boolean.TRUE);
+    public static final ResponseBoolean FALSE = new ResponseBoolean(Boolean.FALSE);
+}
