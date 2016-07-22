1af04c914879dcd67d25e3710d9f67f3369a9160
zhanguo <bupengwen@youzan.com>
2016-07-22 17:28:19 +0800

    删除nsq

--- src/main/java/com/youzan/sz/common/nsq/Consumercallback.java
+++
@@ -1,9 +0,0 @@
-package com.youzan.sz.common.nsq;
-
-/**
- * Created by jinxiaofei on 16/7/21.
- */
-@FunctionalInterface
-public interface ConsumerCallback {
-   <T extends NSQmsgBean> void callback(T message);
-}
