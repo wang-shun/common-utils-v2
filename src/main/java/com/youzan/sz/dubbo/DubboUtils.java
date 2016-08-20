6a0c48f1369293fc12139ef3ecbe84feffe8488e
pan <wangpan@youzan.com>
2016-08-20 14:44:04 +0800

    多注册中心

--- src/main/java/com/youzan/sz/dubbo/DubboUtils.java
+++ src/main/java/com/youzan/sz/dubbo/DubboUtils.java
@@ -1,14 +1,18 @@
 package com.youzan.sz.dubbo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ProtocolConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.ServiceConfig;
 import com.youzan.sz.common.util.SpringUtils;
 import com.youzan.sz.monitor.HeathCheck;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.youzan.sz.common.util.SpringUtils.getBean;
 
 /**
  *
@@ -17,8 +21,8 @@ import com.youzan.sz.monitor.HeathCheck;
 public class DubboUtils {
     private final static Logger LOGGER      = LoggerFactory.getLogger(com.youzan.sz.dubbo.DubboUtils.class);
     static ApplicationConfig    application = null;
-    static RegistryConfig       registry    = null;
-    static ProtocolConfig       protocol    = null;
+    static List<RegistryConfig> registrys    = new ArrayList<>();
+    static List<ProtocolConfig>       protocols   = new ArrayList<>();
 
     // 注意：ServiceConfig为重对象，内部封装了与注册中心的连接，以及开启服务端口
 
@@ -27,12 +31,46 @@ public class DubboUtils {
         // 服务实现
         // 服务提供者暴露服务配置
         ServiceConfig<T> service = new ServiceConfig<T>(); // 此实例很重，封装了与注册中心的连接，请自行缓存，否则可能造成内存和连接泄漏
-        application = SpringUtils.getBean(ApplicationConfig.class);
-        registry = SpringUtils.getBean(RegistryConfig.class);
-        protocol = SpringUtils.getBean(ProtocolConfig.class);
+        application = getBean(ApplicationConfig.class);
+       // registry = SpringUtils.getBean(RegistryConfig.class);
+        try {
+            RegistryConfig zkRegistry = (RegistryConfig) getBean("zookeeper");
+            if (zkRegistry != null) {
+                LOGGER.info("zk Register config add {}", zkRegistry);
+                registrys.add(zkRegistry);
+            }
+            RegistryConfig hauntRegistry = (RegistryConfig) getBean("haunt");
+            if (hauntRegistry != null) {
+                LOGGER.info("etcd Register config add {}", hauntRegistry);
+                registrys.add(hauntRegistry);
+            }
+        }catch (Exception e){
+            LOGGER.info("multi register center error {}",e);
+        }
+
         service.setApplication(application);
-        service.setRegistry(registry); // 多个注册中心可以用setRegistries()
-        service.setProtocol(protocol); // 多个协议可以用setProtocols()
+
+        if(registrys.size() ==0 ){
+            registrys.add(getBean(RegistryConfig.class));
+        }
+        service.setRegistries(registrys); // 多个注册中心可以用setRegistries()
+
+        /*ProtocolConfig dubboProtocol = (ProtocolConfig)SpringUtils.getBean("dubbo");
+        if(dubboProtocol != null){
+            LOGGER.info("dubboProtocol   add {}",dubboProtocol);
+            protocols.add(dubboProtocol);
+        }
+        ProtocolConfig novaProtocol = (ProtocolConfig)SpringUtils.getBean("nova");
+        if(novaProtocol!=null){
+            LOGGER.info("novaProtocol   add {}",novaProtocol);
+            protocols.add(novaProtocol);
+        }
+        */
+        if(protocols.size() ==0 ){
+            protocols.add(SpringUtils.getBean(ProtocolConfig.class));
+        }
+        service.setProtocols(protocols); // 多个协议可以用setProtocols()
+
         service.setInterface(HeathCheck.class);
         service.setRef(t);
         service.setVersion("1.0.0");
