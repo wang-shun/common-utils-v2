919b7721ae9f9cdb7d8757866eabb93d8d7a3060
zhanguo <bupengwen@youzan.com>
2016-08-29 01:05:45 +0800

    增加一个开发模式

--- src/main/java/com/youzan/sz/common/base/simple/BaseServiceImpl.java
+++ src/main/java/com/youzan/sz/common/base/simple/BaseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.youzan.sz.common.base.simple;
 
 import com.youzan.sz.DistributedCallTools.DistributeAttribute;
+import com.youzan.sz.common.interfaces.DevModeEnable;
+import com.youzan.sz.common.interfaces.ToolKits;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +10,6 @@ import org.slf4j.LoggerFactory;
  *
  * Created by zhanguo on 16/8/18.
  */
-public abstract class BaseServiceImpl implements DistributeAttribute {
+public abstract class BaseServiceImpl implements ToolKits {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 }
