d671117d6b57dd8ed790c3fae303d050a846e999
zefa <wangwei@youzan.com>
2016-11-28 18:14:07 +0800

    totalPrice

--- src/main/java/com/youzan/sz/common/util/PriceUtil.java
+++ src/main/java/com/youzan/sz/common/util/PriceUtil.java
@@ -1,11 +1,17 @@
 package com.youzan.sz.common.util;
 
+import com.youzan.sz.common.Common;
+
 import java.math.BigDecimal;
 
 /**
  * Created by YANG on 16/4/22.
  */
 public class PriceUtil {
+
+    private PriceUtil() {
+        throw new IllegalAccessError("Utility class");
+    }
 
     /**
      * 分转元,100分 = 1元
@@ -16,4 +22,16 @@ public class PriceUtil {
     public static String convertF2Y(Long price) {
         return new BigDecimal(price).divide(new BigDecimal(100F)).toString();
     }
+
+    /**
+     * 计算价格 (单价 * 新的数量)
+     *
+     * @param price
+     * @param amount
+     * @return
+     */
+    public static BigDecimal totalPrice(Long price, Long amount) {
+        return new BigDecimal(price * amount).divide(new BigDecimal(Common.QUANTITY_MULTIPLE));
+    }
+
 }
