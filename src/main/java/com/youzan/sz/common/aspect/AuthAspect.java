2b20e15a410d1332b93985c4581c285f9f54dad9
pan <wangpan@youzan.com>
2016-12-06 21:22:23 +0800

    增加新权限拦截切面

--- src/main/java/com/youzan/sz/common/aspect/AuthAspect.java
+++ src/main/java/com/youzan/sz/common/aspect/AuthAspect.java
@@ -1,7 +1,13 @@
 package com.youzan.sz.common.aspect;
 
+import com.youzan.platform.util.lang.StringUtil;
+import com.youzan.sz.DistributedCallTools.DistributedContextTools;
 import com.youzan.sz.common.annotation.Auth;
+import com.youzan.sz.common.model.auth.GrantPolicyDTO;
 import com.youzan.sz.common.permission.PermissionsEnum;
+import com.youzan.sz.common.response.BaseResponse;
+import com.youzan.sz.common.service.AuthService;
+import com.youzan.sz.session.SessionTools;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -10,6 +16,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Resource;
 import java.lang.reflect.Method;
 
 /**
@@ -17,6 +24,9 @@ import java.lang.reflect.Method;
  */
 public class AuthAspect extends BaseAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthAspect.class);
+
+    @Resource
+    private AuthService         authService;
 
     @Pointcut("@annotation(com.youzan.sz.common.annotation.Auth)")
     public void pointcut() {
@@ -32,15 +42,81 @@ public class AuthAspect extends BaseAspect {
         Method method = this.getMethod(pjp);
         Auth auth = method.getAnnotation(Auth.class);
         Class<?> returnType = method.getReturnType();
-
         // 获取注解上传过来的参数
         PermissionsEnum[] allowedPermissions = auth.allowedPermissions();
-        return false;
-    }
-
-    private boolean checkPermission(PermissionsEnum[] allowedPermissions){
-
+        boolean allow = checkPermission(allowedPermissions);
 
         return false;
     }
+
+    /**
+     * 如果有权限 返回true 否则 false
+     * @param allowedPermissions
+     * @return
+     */
+    private boolean checkPermission(PermissionsEnum[] allowedPermissions) {
+
+        if (allowedPermissions == null || allowedPermissions.length == 0) {
+            return true;
+        }
+
+        final Long adminId = DistributedContextTools.getAdminId();
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("auth user permission:adminId:{},yzAccount:{}", adminId,
+                SessionTools.getInstance().get(SessionTools.YZ_ACCOUNT));
+        }
+
+        String userShopId = SessionTools.getInstance().get(SessionTools.SHOP_ID);
+        if (StringUtil.isEmpty(userShopId)) {//店铺不存在
+            LOGGER.warn("shopId can not pass authority,context shopId is empty");
+            return false;
+        }
+        //bid不为空,需要进行bid判断
+        String userBid = SessionTools.getInstance().get(SessionTools.BID);
+        if (StringUtil.isEmpty(userBid)) { //bid不为空,需要进行bid判断
+            LOGGER.warn("bid can not pass authority.context bid is empty");
+            return false;
+        }
+
+        String staffId = SessionTools.getInstance().get(SessionTools.STAFF_ID);
+        if (StringUtil.isEmpty(staffId)) { //bid不为空,需要进行bid判断
+            LOGGER.warn("staffId can not pass authority.context staffId is empty");
+            return false;
+        }
+        GrantPolicyDTO grantPolicyDTO = new GrantPolicyDTO();
+        grantPolicyDTO.setBid(Long.valueOf(userBid));
+        grantPolicyDTO.setShopId(Long.valueOf(Long.valueOf(userShopId)));
+        grantPolicyDTO.setStaffId(Long.valueOf(Long.valueOf(staffId)));
+        BaseResponse<Long[]> response = authService.loadUserPermission(grantPolicyDTO);
+
+        Long[] userPermissions = response.getData();
+        if (userPermissions == null || userPermissions.length == 0) {
+            LOGGER.warn("user permissons is null,adminId:{},yzAccount:{}", adminId,
+                SessionTools.getInstance().get(SessionTools.YZ_ACCOUNT));
+            return false;
+        }else {
+            if(LOGGER.isInfoEnabled()){
+                LOGGER.info("get user permissons:adminId:{},yzAccount:{},permission:{}", adminId,
+                        SessionTools.getInstance().get(SessionTools.YZ_ACCOUNT),userPermissions);
+            }
+        }
+        for (PermissionsEnum permissionsEnum : allowedPermissions) {
+            if ( (permissionsEnum.getPermissionsIndex().getIndex()+1) > userPermissions.length) {
+                LOGGER.warn("interface permissons out of user owner permissions,adminId:{},yzAccount:{},need permission:{},owner permisssions:{}", adminId,
+                        SessionTools.getInstance().get(SessionTools.YZ_ACCOUNT),permissionsEnum,userPermissions);
+                return false;
+            }else {
+                Long needPermission = permissionsEnum.getPermissionsIndex().getValue();
+               if( (userPermissions[permissionsEnum.getPermissionsIndex().getIndex()] & needPermission) != needPermission ) {
+                   LOGGER.warn("interface permissons out of user owner permissions,adminId:{},yzAccount:{},need permission:{},owner permisssions:{}", adminId,
+                           SessionTools.getInstance().get(SessionTools.YZ_ACCOUNT),permissionsEnum,Long.toHexString(needPermission));
+               }else{
+                   return true;
+               }
+
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/youzan/sz/common/permission/PermissionsUtil.java
+++ src/main/java/com/youzan/sz/common/permission/PermissionsUtil.java
@@ -12,9 +12,11 @@ import java.util.stream.Collectors;
  * Created by wangpan on 216/11/30.
  */
 public class PermissionsUtil {
-    private static final Map<PermissionsIndexEnum, PermissionsEnum> rightsMap = new HashMap<>();
-    private static final Logger LOGGER = LoggerFactory.getLogger(PermissionsUtil.class);
-    private static final String RIGHTS_SPLIT=",";
+    private static final Map<PermissionsIndexEnum, PermissionsEnum> rightsMap    = new HashMap<>();
+    private static final Logger                                     LOGGER       = LoggerFactory
+        .getLogger(PermissionsUtil.class);
+    private static final String                                     RIGHTS_SPLIT = ",";
+
     /**
      * 校验重复权限,如果出现重复退出当前jvm
      */
@@ -43,48 +45,56 @@ public class PermissionsUtil {
      * @param collection
      * @return
      */
-    public static  Long[] transferPermissions2LongArr(Collection <PermissionsEnum> collection){
+    public static Long[] transferPermissions2LongArr(Collection<PermissionsEnum> collection) {
 
-        if(CollectionUtils.isEmpty(collection)){
+        if (CollectionUtils.isEmpty(collection)) {
             return null;
         }
-      /*  Map<Integer,List<RightsEnum>> rightsEnumMap = collection.stream().collect(Collectors.groupingBy(new Function<RightsEnum, Integer>() {
+        /*  Map<Integer,List<RightsEnum>> rightsEnumMap = collection.stream().collect(Collectors.groupingBy(new Function<RightsEnum, Integer>() {
             @Override
             public Integer apply(RightsEnum t) {
                 return Integer.valueOf(t.getRightsIndex().getIndex());
             }
         }));
-      */
-        Map<Integer,List<PermissionsEnum>> rightsEnumMap = collection.stream().collect(Collectors.groupingBy(e->e.getPermissionsIndex().getIndex()));
+        */
+        Map<Integer, List<PermissionsEnum>> rightsEnumMap = collection.stream()
+            .collect(Collectors.groupingBy(e -> e.getPermissionsIndex().getIndex()));
         Integer maxIndex = rightsEnumMap.keySet().stream().max(Integer::compare).orElseGet(null);
 
-        Long [] rightArr = new Long[maxIndex+1];
+        Long[] rightArr = new Long[maxIndex + 1];
 
         Set set = rightsEnumMap.entrySet();
         Iterator i = set.iterator();
-        while(i.hasNext()){
-            Map.Entry<Integer, List<PermissionsEnum>> entry=(Map.Entry<Integer, List<PermissionsEnum>>)i.next();
+        while (i.hasNext()) {
+            Map.Entry<Integer, List<PermissionsEnum>> entry = (Map.Entry<Integer, List<PermissionsEnum>>) i.next();
 
-            if(rightArr[entry.getKey().intValue()] == null){
+            if (rightArr[entry.getKey().intValue()] == null) {
                 rightArr[entry.getKey().intValue()] = 0L;
             }
-            for(PermissionsEnum rightsEnum:entry.getValue()){
-                rightArr[entry.getKey().intValue()] = rightsEnum.getPermissionsIndex().getValue()|rightArr[entry.getKey().intValue()];
+            for (PermissionsEnum rightsEnum : entry.getValue()) {
+                rightArr[entry.getKey().intValue()] = rightsEnum.getPermissionsIndex().getValue()
+                                                      | rightArr[entry.getKey().intValue()];
             }
 
         }
         return rightArr;
     }
-    public static String transferPermissions2String(Collection <PermissionsEnum> collection){
 
-        Long [] rights = transferPermissions2LongArr(collection);
-        if(rights == null){
+    /**
+     * 权限long arr=>string
+     * @param collection
+     * @return
+     */
+    public static String transferPermissions2String(Collection<PermissionsEnum> collection) {
+
+        Long[] rights = transferPermissions2LongArr(collection);
+        if (rights == null) {
             return "";
         }
         StringBuilder rightsStr = new StringBuilder();
-        for(Long right:rights){
+        for (Long right : rights) {
 
-            if(rightsStr.length() !=0 ){
+            if (rightsStr.length() != 0) {
                 rightsStr.append(RIGHTS_SPLIT);
             }
             rightsStr.append(right);
@@ -92,39 +102,120 @@ public class PermissionsUtil {
 
         return rightsStr.toString();
     }
-    public static Long[] transferPermissionsStr2LongArr(String rightsStr){
+    /**
+     * 权限str=>long[]
+     * @param permissionsStr
+     * @return
+     */
+    public static Long[] transferPermissionsStr2LongArr(String permissionsStr) {
 
-        if(StringUtils.isEmpty(rightsStr)){
+        if (StringUtils.isEmpty(permissionsStr)) {
             return null;
         }
-        String [] rights = rightsStr.split(RIGHTS_SPLIT);
+        String[] rights = permissionsStr.split(RIGHTS_SPLIT);
 
-        if(rights == null || rights.length == 0){
+        if (rights == null || rights.length == 0) {
             return null;
         }
         List<Long> rightList = new ArrayList<>(rights.length);
-        for(String right:rights){
-            if(StringUtils.isEmpty(right)){
+        for (String right : rights) {
+            if (StringUtils.isEmpty(right)) {
                 rightList.add(0L);
-            }else {
+            } else {
                 rightList.add(Long.valueOf(right));
             }
         }
 
-        return (Long[])rightList.toArray(new Long[rightList.size()]);
+        return (Long[]) rightList.toArray(new Long[rightList.size()]);
     }
 
-    public static void main(String [] ags){
+    /**
+     * 合并角色多个权限
+     * @param permissionsStr
+     * @return
+     */
+    public static Long[] mergePermissionStr2LongArr(String... permissionsStr) {
 
-       // RigthsUtil.checkRightsRepeat();
+        if (permissionsStr == null || permissionsStr.length == 0) {
+            return null;
+        }
+        List<Long[]> permissions = new ArrayList<>();
+        int max = 0;
+        for (String permission : permissionsStr) {
 
-        for(RolesEnum rolesEnum: RolesEnum.values()){
+            Long[] permissonLong = PermissionsUtil.transferPermissionsStr2LongArr(permission);
+            max = permissonLong == null ? max : permissonLong.length > max ? permissonLong.length : max;
+            if (permissonLong == null) {
+                permissions.add(permissonLong);
+            }
+        }
+        Long[] permissionsLong = new Long[max];
+
+        for (int i = 0; i < max; i++) {
+            Long eachLong = 0L;
+            for (Long[] each : permissions) {
+
+                if (each.length >= (i + 1)  && (each[i] != null)){
+                    eachLong = eachLong | each[i];
+                }
+            }
+            permissionsLong[i] = eachLong;
+        }
+        return permissionsLong;
+    }
+
+    /**
+     * 合并角色多个权限
+     * @param permissionsStr
+     * @return
+     */
+    public static String mergePermissionStr2Str(String... permissionsStr) {
+        Long[] allPermisssion = mergePermissionStr2LongArr(permissionsStr);
+        if (allPermisssion == null || allPermisssion.length == 0) {
+            return null;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Long each:allPermisssion){
+            if(stringBuilder.length() ==0){
+                stringBuilder.append(each);
+            }else {
+                stringBuilder.append(RIGHTS_SPLIT+each);
+            }
+        }
+        return stringBuilder.toString();
+        /* List<String> allPermissonStr = Arrays.asList(allPermisssion).stream().map(t -> t.toString())
+            .collect(Collectors.toList());
+        return allPermissonStr.toArray(new String[allPermissonStr.size()]);
+        */
+    }
+    /**
+     * 合并角色多个权限
+     * @param permissionsStr
+     * @return
+     */
+    public static String transferPermissionLongArr2Str(Long... permissionsStr) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Long each:permissionsStr){
+            if(stringBuilder.length() ==0){
+                stringBuilder.append(each);
+            }else {
+                stringBuilder.append(RIGHTS_SPLIT+each);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] ags) {
+
+        // RigthsUtil.checkRightsRepeat();
+
+        for (RolesEnum rolesEnum : RolesEnum.values()) {
 
             String str = transferPermissions2String(rolesEnum.getPermissions());
             System.out.println(str);
             Long[] longr = transferPermissionsStr2LongArr(str);
-            if(longr != null){
-                for(Long r:longr){
+            if (longr != null) {
+                for (Long r : longr) {
                     System.out.println(Long.toHexString(r));
                 }
             }
