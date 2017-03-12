626222828e97798710f69fa18aad569af25950a9
jinxiaofei <jinxiaofei@youzan.com>
2017-03-12 17:33:26 +0800

    update the validateParam aspect3

--- src/main/java/com/youzan/sz/common/aspect/ValidateParamsAspect.java
+++ src/main/java/com/youzan/sz/common/aspect/ValidateParamsAspect.java
@@ -33,6 +33,7 @@ import javax.annotation.Resource;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
+
 /**
  * Created with IntelliJ IDEA.
  * User: Kid
@@ -42,52 +43,57 @@ import javax.validation.Validator;
 
 @Aspect
 public class ValidateParamsAspect extends BaseAspect {
-
+    
     private static final Logger LOGGER = LoggerFactory.getLogger(ValidateParamsAspect.class);
-
-
+    
+    
     @Resource //TODO 注入两次,第二次注入null
     private Validator validator;
-
+    
+    
     @Pointcut("@annotation(com.youzan.sz.common.annotation.ValidateParams)")
     public void pointcut() {
+        
     }
-
+    
+    
     @Before("pointcut()")
     public void before(JoinPoint joinPoint) {
         //TODO 不知道原因，有@Before Advisor，@Around Advisor 才会执行
     }
-
+    
+    
     //检验内部使用传递过来的参数
     @Around("pointcut()")
     public Object handle(ProceedingJoinPoint pjp) {
+        
         if (validator == null) { //TODO Spring加载了两次该类，第一次注入了validator,第二次注入null，暂时采用这样的方式注入
             validator = (Validator) SpringUtils.getBean("validator");
         }
-
+        
         Method method = this.getMethod(pjp);
         ValidateParams validateParamsAnnotation = method.getAnnotation(ValidateParams.class);
         Class<?> returnType = method.getReturnType();
-
+        
         Class[] classes = validateParamsAnnotation.paramClasses();
         Object[] args = pjp.getArgs();
         String[] excludeProperties = validateParamsAnnotation.excludeProperties();
         String[] includeProperties = validateParamsAnnotation.includeProperties();
         //如果注解上面校验参数不写的时候，默认校验所有的类
-        if(classes.length==0){
-            classes=method.getParameterTypes();
+        if (classes.length == 0) {
+            classes = method.getParameterTypes();
         }
-    
-    
-        Set<ConstraintViolation<Object>> constraintSet = validate(classes, args, excludeProperties,includeProperties);
+        
+        
+        Set<ConstraintViolation<Object>> constraintSet = validate(classes, args, excludeProperties, includeProperties);
         if (!constraintSet.isEmpty()) {
             String errors = buildErrorMsg(constraintSet);
             if (BaseResponse.class.isAssignableFrom(returnType)) {
                 return new BaseResponse<>(ResponseCode.PARAMETER_ERROR.getCode(), errors, null);
-            } else {
+            }else {
                 throw new BusinessException((long) ResponseCode.PARAMETER_ERROR.getCode(), "参数错误:\t" + errors);
             }
-        } else {
+        }else {
             try {
                 return pjp.proceed();
             } catch (BusinessException be) {
@@ -97,43 +103,39 @@ public class ValidateParamsAspect extends BaseAspect {
                 LOGGER.warn("Error:", e);
                 if (BaseResponse.class.isAssignableFrom(returnType)) {
                     return new BaseResponse<>(ResponseCode.ERROR.getCode(), "系统异常", e);
-                } else {
+                }else {
                     throw new BusinessException((long) ResponseCode.ERROR.getCode(), "系统异常", e);
                 }
             }
         }
     }
-
+    
+    
     /**
      * 构造参数错误消息
-     *
-     * @param constraintSet
-     * @return
      */
-
+    
     private String buildErrorMsg(Set<ConstraintViolation<Object>> constraintSet) {
+        
         if (constraintSet != null && !constraintSet.isEmpty()) {
             StringBuilder sb = new StringBuilder();
-            constraintSet.forEach(c ->
-                    sb.append(c.getPropertyPath()).append(":").append(c.getMessage()).append(",")
-            );
+            constraintSet.forEach(c -> sb.append(c.getPropertyPath()).append(":").append(c.getMessage()).append(","));
             return sb.deleteCharAt(sb.length() - 1).toString();
-        } else {
+        }else {
             return "";
         }
     }
-
+    
+    
     /**
      * 处理参数,判断是不是符合要求
-     *
-     * @param classes
-     * @param args
-     * @return
      */
-    private Set<ConstraintViolation<Object>> validate(Class[] classes, Object[] args, String[] excludeProperties,String[] includeProperties) {
+    private Set<ConstraintViolation<Object>> validate(Class[] classes, Object[] args, String[] excludeProperties,
+            String[] includeProperties) {
+        
         Set<ConstraintViolation<Object>> constraintSet = new HashSet<>();
         Set<ConstraintViolation<Object>> result = new HashSet<>();
-    
+        
         for (Object obj : args) {
             if (obj instanceof Collection) {
                 Collection collection = (Collection) obj;
@@ -141,47 +143,42 @@ public class ValidateParamsAspect extends BaseAspect {
                     setContextArgs(o);
                     constraintSet.addAll(doValidate(classes, o));
                 }
-            } else {
+            }else {
                 constraintSet.addAll(doValidate(classes, obj));
             }
         }
-        if (includeProperties != null) {
+        if (includeProperties != null && includeProperties.length > 0) {
             for (String includeProperty : includeProperties) {
                 for (ConstraintViolation<Object> violation : constraintSet) {
                     if (violation.getPropertyPath().toString().equals(includeProperty)) {
                         result.add(violation);
-                        break;
                     }
                 }
             }
+        }else {
+            result = constraintSet;
         }
-
+        
         if (excludeProperties != null) {
             for (String excludeProperty : excludeProperties) {
-                for (ConstraintViolation<Object> violation : result) {
-                    if (violation.getPropertyPath().toString().equals(excludeProperty)) {
-                        result.remove(violation);
-                        break;
-                    }
-                }
+                result.removeIf(objectConstraintViolation -> objectConstraintViolation.getPropertyPath().toString()
+                        .equals(excludeProperty));
             }
         }
-       
-
+        
+        
         if (LOGGER.isInfoEnabled()) {
             LOGGER.info("Parameters:{}", JSON.toJSONString(args));
         }
         return result;
     }
-
+    
+    
     /**
      * 检验
-     *
-     * @param classes
-     * @param obj
-     * @return
      */
     private Set<ConstraintViolation<Object>> doValidate(Class[] classes, Object obj) {
+        
         if (classes != null && classes.length > 0) {
             boolean anyMatch = Arrays.stream(classes).anyMatch(Predicate.isEqual(obj.getClass()));
             if (anyMatch) {
@@ -190,32 +187,32 @@ public class ValidateParamsAspect extends BaseAspect {
         }
         return Collections.emptySet();
     }
-
+    
+    
     /**
      * 给方法参数添加必要的上下文中的参数
-     *
-     * @param argument
      */
     private void setContextArgs(Object argument) {
+        
         if (argument instanceof BaseModel) {
             BaseModel baseModel = (BaseModel) argument;
             if (baseModel.getAdminId() == null) {
                 baseModel.setAdminId(DistributedContextTools.getAdminId());
             }
-
+            
             if (Strings.isNullOrEmpty(baseModel.getRequestIp())) {
                 baseModel.setRequestIp(DistributedContextTools.getRequestIp());
             }
-        } else if (argument instanceof BaseDTO) {
+        }else if (argument instanceof BaseDTO) {
             BaseDTO baseDTO = (BaseDTO) argument;
             if (baseDTO.getAdminId() == null) {
                 baseDTO.setAdminId(DistributedContextTools.getAdminId());
             }
-
+            
             if (Strings.isNullOrEmpty(baseDTO.getRequestIp())) {
                 baseDTO.setRequestIp(DistributedContextTools.getRequestIp());
             }
         }
     }
-
+    
 }
