5bec1145fe537a5e3fb5ca3606f5f7746407eb8a
Kid <wanghanyang@youzan.com>
2017-03-15 14:48:28 +0800

    删除多余 配置

--- src/main/java/com/youzan/sz/common/util/test/BaseSpringTest.java
+++ src/main/java/com/youzan/sz/common/util/test/BaseSpringTest.java
@@ -1,9 +1,7 @@
 package com.youzan.sz.common.util.test;
 
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ContextConfiguration;
 
 
 /**
@@ -11,9 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
  * 单元测试专用
  * 如果需要测试远程service,{@link BaseIntTest}
  */
-@RunWith(ExtendedSpringJUnit4ClassRunner.class)
-@SuppressWarnings("SpringContextConfigurationInspection")
-@ContextConfiguration(locations = "classpath:config-spring.xml")
+
 public abstract class BaseSpringTest extends BaseTest {
     
     private final static Logger LOGGER = LoggerFactory.getLogger(BaseSpringTest.class);
