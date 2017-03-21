8a1de43f38c349d4a30423cfee617a0576775221
zhanguo <bupengwen@youzan.com>
2017-03-22 04:04:05 +0800

    修改nsq.默认不需要实现nsqmsg

--- src/main/java/com/youzan/sz/nsq/StringCodec.java
+++ src/main/java/com/youzan/sz/nsq/StringCodec.java
@@ -4,19 +4,21 @@ import java.nio.charset.StandardCharsets;
 
 import com.youzan.sz.common.util.JsonUtils;
 
+
 /**
- *
  * Created by zhanguo on 16/7/29.
  */
-public class StringCodec<T extends NSQMsg, V> extends AbstractNSQCodec<T, V> {
-
+public class StringCodec<T, V> extends AbstractNSQCodec<T, V> {
+    
     public StringCodec() {
     }
-
+    
+    
     public StringCodec(Class<T> t) {
         this.decodeClazz = t;
     }
-
+    
+    
     @Override
     public T decode(byte[] bytes) {
         if (getDecodeClass() == null) {
@@ -28,7 +30,8 @@ public class StringCodec<T extends NSQMsg, V> extends AbstractNSQCodec<T, V> {
         }
         return JsonUtils.json2Bean(new String(bytes, StandardCharsets.UTF_8), getDecodeClass());
     }
-
+    
+    
     @Override
     public byte[] encode(Object obj) {
         return JsonUtils.bean2Json(obj).getBytes(StandardCharsets.UTF_8);
