86b50124fa1912b4392de15c457d22b7ee8d5e80
dufengtao <dufengtao@youzan.com>
2017-10-24 15:19:12 +0800

    增加代理商的业务id

--- src/main/java/com/youzan/sz/common/enums/AppEnum.java
+++ src/main/java/com/youzan/sz/common/enums/AppEnum.java
@@ -3,46 +3,55 @@ package com.youzan.sz.common.enums;
 import java.util.HashMap;
 import java.util.Map;
 
+
 /**
- *
  * Created by zhanguo on 16/8/18.
  */
 public enum AppEnum {
-                     FC(1, "fc", "免费收银"), //freeCheckout
-                     SS(2, "ss", "有赞零售"), //super store
-                     FSS(3, "fss", "有赞零售免费版"), //free super store
-                     PC(11, "fss", "收银专业版"), //Professional Checkout
-                     BEAUTY(21, "beauty", "美业"),
-                     COMERCIAL(31,"comercial","商圈"),
-    ;
-    private Integer                            aid;
-    private String                             shortName;
-    private String                             name;
+    SS(2, "ss", "有赞零售"),
+    FC(1, "fc", "免费收银"),
+    FSS(3, "fss", "有赞零售免费版"),
+    AGENT_PLATFORM(4, "agentPlatform", "商圈"),
+    PC(11, "fss", "收银专业版"),
+    BEAUTY(21, "beauty", "美业"),
+    COMERCIAL(31, "comercial", "商圈");
+    
+    private Integer aid;
+    
+    private String shortName;
+    
+    private String name;
+    
     private final static Map<Integer, AppEnum> AID_MAPS = new HashMap<>();
+    
     static {
         for (AppEnum appEnum : AppEnum.values()) {
             AID_MAPS.put(appEnum.aid, appEnum);
         }
     }
-
+    
     AppEnum(Integer aid, String shortName, String name) {
         this.aid = aid;
         this.shortName = shortName;
         this.name = name;
     }
-
+    
+    
     public static AppEnum getAppByAid(Integer aId) {
         return AID_MAPS.get(aId);
     }
-
+    
+    
     public String getName() {
         return name;
     }
-
+    
+    
     public Integer getAid() {
         return aid;
     }
-
+    
+    
     public String getShortName() {
         return shortName;
     }
